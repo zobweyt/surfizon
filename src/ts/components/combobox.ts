6d05cec8d708879dac199e86b004a40e4ae1d14a
zobweyt <zobweyt@gmail.com>
2023-05-03 00:40:19 +0300

    First commit

---
+++ src/ts/components/combobox.ts
@@ -0,0 +1,67 @@
+import { Dropdown } from './dropdown';
+import { wrap } from '../utils';
+
+export class ComboBox extends Dropdown {
+    protected select!: HTMLSelectElement;
+
+    private get template() {
+        const options = this.select.querySelectorAll('option');
+        const items = Array.from(options).map(option => `
+            <a href="#" class="dropdown-item dropdown-action" data-value="${option.value}">
+                <i class="icon ${option.dataset.icon}"></i>
+                <span>${option.textContent}</span>
+            </a>`
+        );
+
+        return `
+        <button type="button" class="btn dropdown-toggle">
+            ${this.select.dataset.placeholder}
+        </button>
+        <div class="dropdown-menu">
+            ${items.join('')}
+        </div>`;
+    }
+
+    constructor(target: HTMLSelectElement) {
+        super(wrap(target, 'combobox'));
+    }
+
+    protected render(): void {
+        this.select = this.element.querySelector('select')!;
+        this.select.style.display = "none";
+        this.element.classList.add("dropdown");
+        this.element.innerHTML += this.template;
+
+        super.render();
+    }
+
+    protected setup(): void {
+        super.setup();
+
+        this.menu.querySelectorAll('.dropdown-action').forEach(option => {
+            option.addEventListener('click', this.optionClicked.bind(this));
+        });
+    }
+
+    private selectOption(option: HTMLElement) {
+        this.menu.querySelector('.dropdown-item.active')?.classList.remove('active');
+        this.select.value = option.dataset.value!; // ! That would not change the value at all in event.
+        this.control.textContent = option.textContent;
+        option.classList.add('active');
+
+        this.hide();
+    }
+
+    private optionClicked(event: Event): void {
+        event.preventDefault();
+        
+        const option = event.currentTarget as HTMLElement;
+
+        if (!option.classList.contains('active')) {
+            this.selectOption(option);
+
+            const form = this.element.closest('form');
+            form?.dispatchEvent(new Event('change'))
+        }
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+  mode: "development",
+  entry: {
+    main: ['./src/ts/index.ts', './src/scss/main.scss']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  stats: {
+    loggingDebug: ['sass-loader'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+                sassOptions: {
+                  includePaths: ["./src/scss/"],
+                  outputStyle: "compressed",
+                },
+            },
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.tsx',
+      '.ts',
+      '.js'
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin()
+  ]
+};
+
+module.exports = config;
