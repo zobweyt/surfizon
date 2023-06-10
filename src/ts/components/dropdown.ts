3dbe4cb2f3a2f1cbaf81bdaa5f8c2a004d818da8
zobweyt <zobweyt@gmail.com>
2023-06-10 21:46:41 +0300

    fix click inside dropdown control hiding

--- src/ts/components/dropdown.ts
+++ src/ts/components/dropdown.ts
@@ -1,51 +1,92 @@
-import { createPopper } from '@popperjs/core';
-import { popperOptions } from '../utils';
+import {
+  Options,
+  Instance,
+  VariationPlacement,
+  createPopper,
+} from "@popperjs/core";
+import { BaseComponent } from "./base-component";
 
-export class Dropdown {
-    protected element: HTMLElement;
-    protected control!: HTMLElement;
-    protected menu!: HTMLElement;
+const CLASS_NAME_SHOW = "show";
 
-    public get isOpen(): boolean {
-        return this.menu.classList.contains('show');
+const SELECTOR_CONTROL = ".dropdown__toggle";
+const SELECTOR_MENU = ".dropdown__menu";
+
+export class Dropdown extends BaseComponent {
+  protected control!: HTMLElement;
+  protected menu!: HTMLElement;
+  protected popper!: Instance;
+
+  public get isOpen(): boolean {
+    return this.menu.classList.contains(CLASS_NAME_SHOW);
+  }
+
+  protected render(): void {
+    this.control = this.element.querySelector(SELECTOR_CONTROL)!;
+    this.menu = this.element.querySelector(SELECTOR_MENU)!;
+    this.popper = createPopper(this.control, this.menu, this.popperOptions);
+  }
+
+  protected initialize(): void {
+    this.control.onclick = this.toggle.bind(this);
+  }
+
+  public show(): void {
+    this.menu.classList.add(CLASS_NAME_SHOW);
+    this.popper.update();
+
+    document.addEventListener("click", this.close.bind(this));
+  }
+
+  public hide(): void {
+    this.menu.classList.remove(CLASS_NAME_SHOW);
+    this.popper.update();
+
+    document.removeEventListener("click", this.close.bind(this));
+  }
+
+  public toggle(event: MouseEvent): void {
+    this.isOpen ? this.hide() : this.show();
+  }
+
+  private close(event: MouseEvent): void {
+    const clickedNode = event.target as Node;
+    const isNodeInMenu = this.menu.contains(clickedNode);
+    const isNodeInControl = this.control.contains(clickedNode);
+    const clickedOutsideDropdown = !isNodeInMenu && !isNodeInControl;
+
+    if (clickedOutsideDropdown) {
+      this.hide();
     }
+  }
 
-    constructor(target: HTMLElement) {
-        this.element = target;
+  private get popperOptions(): Options {
+    const placement = this.element.dataset?.placement as VariationPlacement;
 
-        this.render();
-        this.setup();
-    }
+    const popperOptions: Options = {
+      placement: placement ?? "bottom-start",
+      strategy: "absolute",
+      modifiers: [
+        {
+          name: "computeStyles",
+          options: {
+            adaptive: false,
+          },
+        },
+        {
+          name: "offset",
+          options: {
+            offset: [0, 5],
+          },
+        },
+        {
+          name: "flip",
+          options: {
+            allowedAutoPlacements: ["top"],
+          },
+        },
+      ],
+    };
 
-    protected render(): void {
-        this.control = this.element.querySelector('.dropdown__toggle')!;
-        this.menu = this.element.querySelector('.dropdown__menu')!;
-
-        createPopper(this.control, this.menu, popperOptions);
-    }
-
-    protected setup(): void {
-        this.control.addEventListener('click', this.toggle.bind(this));
-        document.addEventListener('click', this.documentClicked.bind(this));
-    }
-
-    public toggle(): void {
-        this.isOpen ? this.hide() : this.show();
-    }
-
-    public show(): void {
-        this.menu.classList.add('show');
-    }
-
-    public hide(): void {
-        this.menu.classList.remove('show');
-    }
-
-    private documentClicked(event: MouseEvent): void {
-        const target = event?.target as HTMLElement;
-
-        if (!this.menu.contains(target) && this.control != target) {
-            this.hide();
-        }
-    }
+    return popperOptions;
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,47 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const config = {
   mode: "development",
   entry: {
-    main: ['./src/ts/index.ts', './src/scss/main.scss']
+    main: ["./src/ts/index.ts", "./src/scss/main.scss"],
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
   },
   stats: {
-    loggingDebug: ['sass-loader'],
+    loggingDebug: ["sass-loader"],
   },
   module: {
     rules: [
       {
         test: /\.ts(x)?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/
+        loader: "ts-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.scss$/,
         use: [
           MiniCssExtractPlugin.loader,
-          'css-loader',
+          "css-loader",
           {
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: {
-                sassOptions: {
-                  includePaths: ["./src/scss/"],
-                  outputStyle: "compressed",
-                },
+              sassOptions: {
+                includePaths: ["./src/scss/"],
+                outputStyle: "compressed",
+              },
             },
-          }
-        ]
-      }
-    ]
+          },
+        ],
+      },
+    ],
   },
   resolve: {
-    extensions: [
-      '.tsx',
-      '.ts',
-      '.js'
-    ]
+    extensions: [".tsx", ".ts", ".js"],
   },
-  plugins: [
-    new MiniCssExtractPlugin()
-  ]
+  plugins: [new MiniCssExtractPlugin()],
 };
 
 module.exports = config;
